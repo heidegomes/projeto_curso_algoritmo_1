c8dca49430c738d1f485eb94850cc2ace740f33a
heidegomes <heidegc@hotmail.com>
2020-08-11 15:07:33 -0300

    Primeiro commit do cógigo

---
+++ concatenacao.js
@@ -0,0 +1,22 @@
+/*
+Var
+// Seção de Declarações das variáveis 
+   nome: caractere
+   numero: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   escreval("Digite o nome:")
+   leia(nome)
+   escreval("Digite o número:")
+   leia(numero)
+   
+   escreval(nome, " : ", numero)
+*/
+
+var nome, numero
+
+nome = prompt("Digite o nome")
+nome = prompt("Digite o número")
+
+document.getElementById("paragrafo").innerText = nome + " : " + numero
---
+++ estrutura-repita.js
@@ -0,0 +1,30 @@
+/*
+Var
+// Seção de Declarações das variáveis 
+   sairLoop: caractere
+   valor01, valor02: real
+   
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   repita
+         escreva("Digite o primeiro valor: ")
+         leia(valor01)
+         escreva("Digite o segundo valor: ")
+         leia(valor02)
+         escreval("Resultado: ", valor01 + valor02)
+         escreval("Deseja sair? S/N")
+         leia(sairLoop)
+   
+   */
+
+  function acaoBotao() {
+      var sairLoop, valor01, valor02
+      do{
+        valor01 = prompt("Digite o primeiro valor: ")
+        valor02 = prompt("Digite o segundo valor: ")
+        document.getElementById("paragrafo").innerText = "Resultado: " + (ParseInt ( valor01 ) + ( ParseInt ( valor02) )
+        sairLoop = prompt("Deseja sair? S/N")
+      }while(sairLoop == "N")  
+}
+
